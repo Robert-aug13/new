fb6a8699d3c7be8dbed705b99d96088733684f7a
Robert-aug13 <83607960+Robert-aug13@users.noreply.github.com>
2023-07-01 21:42:40 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -63,3 +63,30 @@ resource "null_resource" "enable_microk8s_dashboard" {
   }
 
 }
+
+
+resource "null_resource" "enable_microk8s_metallb" {
+  depends_on = [null_resource.install_microk8s]
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      host        = "18.217.119.180"
+      user        = "ubuntu"
+      private_key = file("C:/Users/Robert/Downloads/micro-new.pem")
+    }
+    inline = [
+      "microk8s enable metallb:${var.metallb_ip_range[0]}",
+    ]
+  }
+}
+
+
+
+variable.tf
+
+variable "metallb_ip_range" {
+  description = "The IP range for MetalLB"
+  type        = list(any)
+  default     = ["172.31.0.222-172.31.0.240"]  # Modify this with your desired IP range
+}
